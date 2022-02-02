d3c713841dc0f0c1876c49ca0218eb02ba7f4b06
James <jameshuang337@gmail.com>
2022-02-01 17:34:24 -0800

    completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,14 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(name: "Burnaby", annual_revenue:300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+burnaby.save
+richmond.save
+gastown.save
+
+# Output (puts) the number of the stores using ActiveRecord's count method,
+puts Store.count
